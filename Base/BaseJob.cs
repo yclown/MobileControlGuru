d0f8cc4f37a3825ceb2c67b4b8e447f705c5a0ce
yclown <yclown@163.com>
2024-08-10 13:37:45 +0800

    Create BaseJob.cs

---
+++ Base/BaseJob.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileControlGuru.Base
+{
+    public class BaseJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            JobKey key = context.JobDetail.Key; 
+            JobDataMap dataMap = context.JobDetail.JobDataMap; 
+            string jobSays = "";
+            dataMap.TryGetString("jobSays", out jobSays);
+
+            return Task.Factory.StartNew(() =>
+            {
+               
+                Console.WriteLine(jobSays + " -- " + DateTime.Now.ToString());
+
+            });
+        }
+    }
+}
